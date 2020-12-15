17a34cdbceb0d0401dba981bcacf15ffb32b5413
Sarjono <sjn060370@gmail.com>
2020-12-16 00:04:25 +0700

    tampilkan semua anggota arisan

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi Rest API berjalan lancar",res);
 };
+
+//menampilkan data nama anggota arisan
+exports.tampilsemuaanggota = function (req, res) {
+    connection.query('SELECT * FROM nama_anggota', function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
