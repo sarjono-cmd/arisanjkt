ebeb1339e3caa962dedad20728072255485b6afb
Sarjono <sjn060370@gmail.com>
2020-12-16 22:29:37 +0700

    menampilkan nama anggota arisan berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,16 @@ exports.tampilsemuaanggota = function (req, res) {
         }
     });
 };
+
+//menampilkan semua data mahasiwa berdasarkan id
+exports.tampilsemuaanggotaid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM nama_anggota WHERE id_anggota = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
--- routes.js
+++ routes.js
@@ -8,4 +8,7 @@ module.exports = function(app){
 
     app.route('/tampil')
         .get(jsonku.tampilsemuaanggota);
+
+        app.route('/tampil/:id')
+        .get(jsonku.tampilsemuaanggotaid);
 }
